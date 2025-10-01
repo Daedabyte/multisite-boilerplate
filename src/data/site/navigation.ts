c7a7385247f5cbaff277349cff136f59717d2f9b
Alejandro Marquez <alejandromarquez@live.com>
2025-10-01 14:18:54 -0400

    feat(app): Added Mobile Navigation and Hero Components

--- src/data/site/navigation.ts
+++ src/data/site/navigation.ts
@@ -1,20 +1,53 @@
 /**
- * Navigation Item
- * @description Defines the structure for navigation items used throughout the site.
- * @interface NavItem
- * @property {string} label - The display text for the navigation item.
- * @property {string} url - The URL the navigation item points to.
- * @property {boolean} [global] - Indicates if the item is part of the global navigation (aka. Main Navigation Bar).
- * @property {boolean} [external] - Indicates if the link is external (opens in a new tab).
- * @property {Record<string, NavItem[]>} [children] - An optional object containing child navigation items, categorized by section.
+ * Site Navigation Configuration
+ * @description This module defines the structure of the site's navigation, including main and footer navigation items.
+ * @module navigation
  */
 
-export interface NavItem {
+/**
+ * Type Definition for NavItem
+ * @typedef {Object} NavItem - Represents a navigation item with optional children.
+ * @property {string} label - The display label for the navigation item.
+ * @property {boolean} [global] - Indicates if the item is part of the global navigation.
+ * @property {Record<string, NavItem>} [children] - An object containing child navigation items.
+ * 
+ * @typedef {Object} NavLink - Represents a navigation link item.
+ */
+interface NavItem {
     label: string;
-    url: string;
     global?: boolean;
+    children ?: Record<string, NavItem>;
+}
+
+/**
+ * Type Definition for NavLink
+ * @typedef {Object} NavLink - Represents a navigation link item extending NavItem.
+ * @property {string} url - The URL the navigation link points to.
+ * @property {boolean} [external] - Indicates if the link is external.
+ * @property {Record<string, NavItem | NavLink>} [children] - An object containing child navigation items or links.
+ * @extends {NavItem}
+ */
+interface NavLink extends NavItem {
+    url: string;
     external?: boolean;
-    children?: Record<string, NavItem>;
+    children?: Record<string, NavItem | NavLink>;
+}
+
+/**
+ * Type Guard for NavLink
+ * @function isNavItem
+ * @param {NavItem | NavLink} item - The item to check.
+ * @return {item is NavLink} - Returns true if the item is a NavLink, false otherwise.
+ */
+export function isNavLink(item: NavItem): item is NavLink {
+    return (item as NavLink).url !== undefined;
+}
+
+export function asNavLink(item: NavItem) : NavLink | null {
+    if (isNavLink(item)) {
+        return item as NavLink;
+    }
+    return null;
 }
 
 /**
@@ -23,7 +56,7 @@ export interface NavItem {
  * @constant {Record<string, NavItem>} navigation - The navigation object containing various navigation items.
  */
 
-export const navigation: Record<string, NavItem> = {
+export const navigation: Record<string, NavItem | NavLink> = {
     "home": {
         label: "Home",
         url: "/",
@@ -31,17 +64,45 @@ export const navigation: Record<string, NavItem> = {
     "about": {
         label: "About",
         url: "/about",
-        global: true
+        global: true,
+        children: {
+            "team": {
+                label: "Team",
+                url: "/about/team",
+                global: true,
+            },
+            "careers": {
+                label: "Careers",
+                url: "/about/careers",
+                global: true,
+            }
+        }
     },
     "documentation": {
         label: "Docs",
-        url: "/docs",
         global: true,
         children: {
-            "colors": {
-                label: "Colors",
-                url: "/docs/colors",
-                global: true
+            "style-guide": {
+                label: "Style Guide",
+                url: "/docs/style-guide",
+                global: true,
+                children: {
+                    "typography": {
+                        label: "Typography",
+                        url: "/docs/style-guide/typography",
+                        global: true,
+                    },
+                    "colors": {
+                        label: "Colors",
+                        url: "/docs/style-guide/colors",
+                        global: true,
+                    },
+                }
+            },
+            "components": {
+                label: "Components",
+                url: "/docs/components",
+                global: true,
             }
         }
     },
